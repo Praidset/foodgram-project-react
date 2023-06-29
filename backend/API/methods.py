ea14a982464decf7c92d9355fcc3f17a15c566f3
unknown <praidset@yandex.ru>
2023-06-30 02:29:41 +0300

    2nd review

--- backend/API/methods.py
+++ backend/API/methods.py
@@ -6,10 +6,8 @@ def CreateUpdate(ingredients, instance=None, recipe=None):
         recipe = instance
     else:
         recipe = recipe
-    Recipeingredient.objects.bulk_create(
-        [Recipeingredient(
-                        recipe=recipe,
-                        ingredients=Ingredient.objects.get(id=ing['id']),
-                        amount=ing['amount']
-        ) for ing in ingredients]
-    )
+    Recipeingredient.objects.bulk_create([Recipeingredient(
+        recipe=recipe,
+        ingredients=Ingredient.objects.get(id=ing['id']),
+        amount=ing['amount']
+    ) for ing in ingredients])
